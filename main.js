55c57c31631d5722d0e9603c9e22b2a7bdc7ba43
Athena112233 <43796329+Athena112233@users.noreply.github.com>
2021-03-07 21:02:20 -0800

    completed demo

--- main.js
+++ main.js
@@ -1,4 +1,22 @@
 function render_demo(demo){
-    console.log(demo);
-    document.getElementById("classification").innerHTML = 'Masked';
+    //case 1
+    if(demo == "case1"){
+        document.getElementById("classification").innerHTML = 'Incorrectly Masked';
+        document.getElementById("original_image").src = "data/case1.jpg";
+        document.getElementById('gradcam_output').src = "data/case1_gradcam.jpg";
+        document.getElementById('ig_output').src = "data/case1_results.jpg";
+    }
+    else if(demo == "case2"){
+        document.getElementById("classification").innerHTML = 'Missing Mask';
+        document.getElementById("original_image").src = "data/case2.jpg";
+        document.getElementById('gradcam_output').src = "data/case2_gradcam.jpg";
+        document.getElementById('ig_output').src = "data/case2_results.jpg";
+    }
+    else if(demo == "case3"){
+        document.getElementById("classification").innerHTML = 'Correctly Masked';
+        document.getElementById("original_image").src = "data/case3.jpg";
+        document.getElementById('gradcam_output').src = "data/case3_gradcam.jpg";
+        document.getElementById('ig_output').src = "data/case3_results.jpg";
+    }
+
 }
